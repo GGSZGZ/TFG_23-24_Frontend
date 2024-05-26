b9899001346d4be1356503ac3284cb497deddb91
IvanSeguraRomero <a27312@svalero.com>
2024-05-26 19:28:22 +0200

    Cart & PaymentMethod Interface done

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,6 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import GameView from '../views/GameView.vue'
-import PaymentView from '../views/PaymentView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
